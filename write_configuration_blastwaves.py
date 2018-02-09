4b1c11fe5bc0787d249c12b7677a5be06766d042
Bert Vandenbroucke <bert.vandenbroucke@gmail.com>
2018-02-09 14:41:20 +0000

    Added configuration script for interacting blast waves test.

---
+++ write_configuration_blastwaves.py
@@ -0,0 +1,22 @@
+# set up for the Sod shock test
+
+import get_cmake_command
+
+sod_options = {
+"rmin_in_au": 0.,
+"rmax_in_au": 6.68459e-12,
+"ncell": 1000,
+"gamma": 1.4,
+"maxtime_in_yr": 1.2049721e-9,
+"number_of_snaps": 10,
+"ic": "IC_BLASTWAVES",
+"eos": "EOS_IDEAL",
+"boundaries": "BOUNDARIES_REFLECTIVE",
+"potential": "POTENTIAL_NONE",
+"unit_mass_in_si": 1.,
+"unit_length_in_si": 1.,
+"g_internal": 6.67408e-11,
+"courant_factor": 0.01
+}
+
+print get_cmake_command.get_cmake_command(sod_options)
